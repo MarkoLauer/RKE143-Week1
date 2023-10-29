cd45ee6ec1fc1cc1c6b6c951291f668497705886
Marko <marko.lauer@gmail.com>
2023-10-29 21:20:08 +0200

    Week 1 code

---
+++ server.js
@@ -0,0 +1,10 @@
+
+let firstName = 'Harry';
+let lastName = "Potter";
+
+//console.log ("Hello, " + firstName + "!");
+//template literals
+//console.log(`Hello, ${firstName}!`);
+
+let greeting = `Hello ${firstName} ${lastName}!`;
+console.log(greeting);
